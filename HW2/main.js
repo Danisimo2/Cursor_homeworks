a5c959727eac8cbf9e4c5abcc1c2fcf7e73a7d41
Danylo <sukhiy37078@gmail.com>
2020-07-19 00:48:53 +0300

    hw

--- HW2/main.js
+++ HW2/main.js
@@ -1,35 +1,28 @@
 let enterFirstNum = Math.floor(prompt('What is your first number?'));
 let numberOne = parseInt(enterFirstNum);
-
 while(!enterFirstNum){
-	enterFirstNum = Number(prompt('Please, enter integer number'));
+    enterFirstNum = Number(prompt('Please, enter integer number'));
 }
-
 let enterSecondNum = Math.floor(prompt('What is your second number?'));
-
 while(!enterSecondNum || enterFirstNum >= enterSecondNum){
-	enterSecondNum = Number(prompt('Please, enter integer number and more than first number'));
+    enterSecondNum = Number(prompt('Please, enter integer number and more than first number'));
 }
-
 let scapeEvenNum = confirm('Scape even number?')
 let sumAllNumbers = 0;
-
-while(numberOne <= enterSecondNum){
-	if (scapeEvenNum){
-		if (numberOne % 2 !== 0){
-			sumAllNumbers += numberOne;
-		}numberOne++;
-	} else{
-		sumAllNumbers += numberOne;
-		numberOne++;
-	}
+while (numberOne <= enterSecondNum) {
+    if(scapeEvenNum) {
+        if(numberOne % 2 === 0) {
+            numberOne++;
+            continue;
+        }
+    }
+    sumAllNumbers += numberOne;
+    numberOne++;
 }
-
 console.log(sumAllNumbers);
-
 const result = `
-	<p>first number: ${enterFirstNum}</p>
-	<p>second number: ${enterSecondNum}</p>
-	<p>sum all numbers: ${sumAllNumbers}</p>
+   <p>first number: ${enterFirstNum}</p>
+   <p>second number: ${enterSecondNum}</p>
+   <p>sum all numbers: ${sumAllNumbers}</p>
 `
 document.getElementById("res").innerHTML = result;
