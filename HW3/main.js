7aae15efa4d1753349c5f127d024bb5fc55000a2
Danylo <sukhiy37078@gmail.com>
2020-07-24 12:20:49 +0300

    hw

--- HW3/main.js
+++ HW3/main.js
@@ -1,21 +1,26 @@
+debugger;
 function getMaxNumber(num){
-	return num = String(num).split('').sort().reverse()
+	return Number(num.toString().split('').sort().pop());
 }
-document.writeln(`<p>Function #1 max number:  ${getMaxNumber(14532564682)[0]}</p>`);
+document.writeln(`<p>Function #1 max number:  ${getMaxNumber(14532564682955647898)}</p>`);
 
-function pow(x, n){
-	let number = 1;
-	for(let i = 0; i < n; i++){
-		number *= x;
-	}if(x === 0 && n === 0) return 'it is an unsolved mystery';
-	else if(n === 0) return 1;
-	else if(x === 0) return 0;
-	else if(n < 0){
-		for(let i = 0; i < -n; i++){
-		number *= x;
-		} return number = 1/number;
-	} else return number;
+function pow(x, n) {
+  let number = 1;
+  for (let i = 0; i < n; i++) {
+    number *= x;
+  }
+  if (x === 0 && n === 0) return "it is an unsolved mystery";
+  else if (n === 0) return 1;
+  else if (x === 0) return 0;
+  else if (n < 0) {
+    for (let i = 0; i < -n; i++) {
+      number *= x;
+    }
+    return (number = 1 / number);
+  } 
+  else return number;
 }
+
 document.writeln(`<p>Function #2 number in power: ${pow(2, -3)}</p>`);
 
 function removeName(name){
@@ -24,25 +29,24 @@ function removeName(name){
 document.writeln(`<p>Function #3 remove name: ${removeName('daNylo')}</p>`);
 
 function convertMoney(howMuch){
-	let suma;
+	let total;
 	const priceForDollar = 25;
-	howMuch = howMuch.toLowerCase();
-	if(howMuch.indexOf('$') === -1 && howMuch.indexOf('uah') === -1){
+	const lowerCase = howMuch.toLowerCase();
+	if(lowerCase.indexOf('$') === -1 && lowerCase.indexOf('uah') === -1){
 		return 'wrong currency';
-	} else if(howMuch.indexOf('uah') !== -1){
-		return suma = howMuch.replace('uah','') / priceForDollar + '$';
-	} else if(howMuch.indexOf('$') !== -1){
-		return suma = howMuch.replace('$','') * priceForDollar + 'UAH';
+	} else if(lowerCase.indexOf('uah') !== -1){
+		return total = lowerCase.replace('uah','') / priceForDollar + '$';
+	} else if(lowerCase.indexOf('$') !== -1){
+		return total = lowerCase.replace('$','') * priceForDollar + 'UAH';
 	} 
-	return suma;
 }
-document.writeln(`<p>Function #4 convert money: ${convertMoney('250$')}</p>`);
+document.writeln(`<p>Function #4 convert money: ${convertMoney('200$')}</p>`);
 
 function getRandomPassword(numberDigits = 8){
-	let pass = [];
+	const pass = [];
 	for(let i = 0; i < numberDigits; i++){
 		let value = Math.floor(Math.random()*10);
-		pass.push(`${value}`);
+		pass.push(value);
 	}
 	return pass.join('');
 }
@@ -56,12 +60,10 @@ document.writeln(`<p>Function #6 is words palindrom?: ${isPalindrom('я несу
 
 function getCounter(word, letter){
 	let count = 0;
-	word = word.toLowerCase();
-	letter = letter.toLowerCase();
-	for(let i = 0; i < word.length; i++){
-		if(word[i] === letter){
-			count++;
-		}
+	const lowerWord = word.toLowerCase();
+	const lowerLetter = letter.toLowerCase();
+	for(let i = 0; i < lowerWord.length; i++){
+		lowerWord[i] === lowerLetter && count++;
 	}
 	return count;
 }
